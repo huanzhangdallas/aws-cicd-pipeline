1d4d363a44d96d95a5c9910182a3439f85fb9318
huanzhangdallas <59899308+huanzhangdallas@users.noreply.github.com>
2021-07-17 23:14:07 -0500

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform{
     backend "s3" {
-        bucket = "pipeline-state-huanzhang"
+        bucket = "pipeline-state-huanzhang1"
         encrypt = true
         key = "terraform.tfstate"
         region = "us-east-1"
